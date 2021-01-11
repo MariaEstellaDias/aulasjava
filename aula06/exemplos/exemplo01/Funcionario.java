9b22346df0d9c9c72b891a356262be95917af92c
MariaEstellaDias <mariaestelladias@gmail.com>
2021-01-11 12:13:00 -0300

    add aula 6 completa

--- aula06/exemplos/exemplo01/Funcionario.java
+++ aula06/exemplos/exemplo01/Funcionario.java
@@ -3,27 +3,42 @@ package exemplos.exemplo01;
 /**
  * Funcionario
  */
-public class Funcionario {
+public class Funcionario { //extends Object { //implicito
     // atributos
     private String nome;
     private double salario;
 
     // método
-    public Funcionario(){} //construtor default
+    public Funcionario() {
+    } // construtor default
+
     public Funcionario(String nome, double salario) {
         this.nome = nome;
-        this.salario = salario;
+        setSalario(salario);
     }
     // método
 
     public String exibir() {
         return nome + ": " + salario;
     }
-    public double getSalario(){
+
+    public double getSalario() {
         return salario;
     }
 
-    public void setSalario(double salario) {
-        this.salario = salario;
+    protected void setSalario(double salario) {
+        if (salario > 0) {
+            this.salario = salario;
+        }
+
+    }
+
+    @Override
+    public String toString() {
+        return exibir();
+    }
+
+    public void aumentarSalario(double taxa) {
+        salario = salario + salario * taxa;
     }
 }
--- aula06/exemplos/exemplo01/Gerente.java
+++ aula06/exemplos/exemplo01/Gerente.java
@@ -1,12 +1,21 @@
 package exemplos.exemplo01;
 
-
-
 public class Gerente extends Funcionario {
     private int nFuncionarios;
 
-    public Gerente(String nome, double salario, int Funcionario){
-        super(nome, salario); //chamada ao construtor da classe "Funcionario"
+    public Gerente(String nome, double salario, int nFuncionarios) {
+        super(nome, salario); // chamada ao construtor da classe "Funcionario"
         this.nFuncionarios = nFuncionarios;
+        setSalario(salario);
+    }
+
+    @Override
+    public String exibir() {
+        return super.exibir() + " - " + nFuncionarios;
+    }
+
+    @Override
+    public void aumentarSalario(double taxa) {
+        super.aumentarSalario(taxa + 0.05);
     }
 }
